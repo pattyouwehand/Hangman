3b72fa064c6a3cdd04d1e9e4b17b368a56176fe9
pattyouwehand <pattyouwehand@gmail.com>
2019-08-13 14:21:45 +0200

    step2

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function wrongGuessCount(word, guesses){
     .length
 }
 console.log('wrong guesses:', wrongGuessCount("hello", ['k', 'l', 'd']), 'should be:', 2)
+
+
+function showGuess(word, guesses){
+    return word
+    .split('')
+    .map(letter => guesses.indexOf(letter) < 0 ? "_" : letter)
+    .join(' ')
+}
+
+console.log('show guess 1:', showGuess('hello', ['l']), 'should be:', '_ _ l l _')
+console.log('show guess 2:', showGuess('hello', ['l', 'a', 'e']), 'should be:', '_ e l l _')
