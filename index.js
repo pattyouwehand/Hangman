d516280306b42593ce3d5f3490761801ecbac395
pattyouwehand <pattyouwehand@gmail.com>
2019-08-13 17:02:53 +0200

    step4

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function wrongGuessCount(word, guesses){
     .filter(letter => word.indexOf(letter) <0)
     .length
 }
-console.log('wrong guesses:', wrongGuessCount("hello", ['k', 'l', 'd']), 'should be:', 2)
-
 
 function showGuess(word, guesses){
     return word
@@ -13,9 +11,6 @@ function showGuess(word, guesses){
     .join(' ')
 }
 
-console.log('show guess 1:', showGuess('hello', ['l']), 'should be:', '_ _ l l _')
-console.log('show guess 2:', showGuess('hello', ['l', 'a', 'e']), 'should be:', '_ e l l _')
-
 function isWinner(word, guesses){
     return word
     .split('')
@@ -23,5 +18,35 @@ function isWinner(word, guesses){
     .length === word.length;
 }
 
-console.log('winner 1:', isWinner('hello', ['e', 'x']), 'should be:', false)
-console.log('winner 2:', isWinner('hello', ['o', 'l', 'e', 'h']), 'should be:', true)
+function ignoreDuplicateLetters(word, guesses){
+    return word
+    .split('')
+}
+
+const readline = require('readline')
+const rl = readline.createInterface({input:process.stdin, output:process.stdout})
+
+function next(word, guesses) {
+    let numWrongGuesses = wrongGuessCount(word, guesses);
+    if(numWrongGuesses === 6){
+        console.log("You've lost!")
+    }else if(isWinner(word, guesses)){
+        console.log("You won!")
+    }else{
+        rl.question('next letter? ', answer => {
+            const letter = answer.trim()[0];
+            console.log('player wrote:', answer)
+            next(word, guesses.concat([ letter ]));
+        });
+    }    
+}
+
+next('hello', [])
+
+
+//node . will run your file in terminal
+//console.log('winner 1:', isWinner('hello', ['e', 'x']), 'should be:', false)
+//console.log('winner 2:', isWinner('hello', ['o', 'l', 'e', 'h']), 'should be:', true)
+//console.log('show guess 1:', showGuess('hello', ['l']), 'should be:', '_ _ l l _')
+//console.log('show guess 2:', showGuess('hello', ['l', 'a', 'e']), 'should be:', '_ e l l _')
+//console.log('wrong guesses:', wrongGuessCount("hello", ['k', 'l', 'd']), 'should be:', 2)
