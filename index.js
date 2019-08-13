127978d0557c63ac3c7508f7e89ec4da3edb23b6
pattyouwehand <pattyouwehand@gmail.com>
2019-08-13 14:44:45 +0200

    step3

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function showGuess(word, guesses){
 
 console.log('show guess 1:', showGuess('hello', ['l']), 'should be:', '_ _ l l _')
 console.log('show guess 2:', showGuess('hello', ['l', 'a', 'e']), 'should be:', '_ e l l _')
+
+function isWinner(word, guesses){
+    return word
+    .split('')
+    .filter(letter => guesses.indexOf(letter) >= 0)
+    .length === word.length;
+}
+
+console.log('winner 1:', isWinner('hello', ['e', 'x']), 'should be:', false)
+console.log('winner 2:', isWinner('hello', ['o', 'l', 'e', 'h']), 'should be:', true)
